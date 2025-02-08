b2791b065dedeb26bab1fe8e5525e4a60b4d4fd9
SolidaoStudio <LuanS.almeida@outlook.com>
2025-02-08 11:05:47 -0300

    day 02 - fix

--- 02 - Clock/script.js
+++ 02 - Clock/script.js
@@ -1,21 +1,18 @@
 function getTime(){
-  const date = new Date();
+  const now = new Date();
 
-  const hour = date.getHours();
-  const hourHandDeg = ((hour/12) * 360)
-  document.querySelector('.hour-hand').style.transform = `rotate(${hourHandDeg}deg)`;
-
-
-  const minute = date.getMinutes();
-  const minHandDeg = ((minute/60) * 360)
-  document.querySelector('.minute-hand').style.transform = `rotate(${minHandDeg}deg)`;
-
-  const second = date.getSeconds();
+  const second = now.getSeconds();
   const secHandDeg = ((second/60) * 360)
   document.querySelector('.second-hand').style.transform = `rotate(${secHandDeg}deg)`;
 
-  console.log(second);
-}
+  const minute = now.getMinutes();
+  const minHandDeg = ((minute/60) * 360) + ((second/60)*6);
+  document.querySelector('.minute-hand').style.transform = `rotate(${minHandDeg}deg)`;
 
+  const hour = now.getHours();
+  const hourHandDeg = ((hour % 12) / 12) * 360 + (minute / 60) * 30;
+  document.querySelector('.hour-hand').style.transform = `rotate(${hourHandDeg}deg)`;
+}
+getTime();
 setInterval(getTime, 1000);
 
